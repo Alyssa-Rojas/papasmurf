a14bcf68f902ce07976a59d35496e090accec653
Alyssa Rojas <111326543+Alyssa-Rojas@users.noreply.github.com>
2023-03-21 21:56:18 +0000

    Tue, Mar 21, 2023, 9:56 PM +00:00

--- js/counter.js
+++ js/counter.js
@@ -4,12 +4,15 @@ counters.forEach(counter => {
     counter.innerText = '0'
 
     const updateCounter = () => {
-        const target = counter.getAttribute('data-target')
+        const target = +counter.getAttribute('data-target')
         const c = +counter.innerText
-        const increment = 5
+        const increment = target / 200
 
         if (c < target) {
             counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
         }
     }
 
