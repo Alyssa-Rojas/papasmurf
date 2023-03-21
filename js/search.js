e06b48f52927b3c8ad0dd1f9cc19fc95809ade8b
Alyssa Rojas <111326543+Alyssa-Rojas@users.noreply.github.com>
2023-03-21 22:00:57 +0000

    Tue, Mar 21, 2023, 10:00 PM +00:00

---
+++ js/search.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('.btn')
+const button = document.querySelector('.search')
+
+search.addEventListener('mouseenter', () => {
+    search.classList.toggle('active')
+})
+
+search.addEventListener('mouseleave', () => {
+    search.classList.toggle('active')
+})
