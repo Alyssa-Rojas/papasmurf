3204e7a8c5382ffb0819a7d5db0f80b8f5e0b718
Alyssa Rojas <111326543+Alyssa-Rojas@users.noreply.github.com>
2023-03-24 22:15:13 +0000

    Fri, Mar 24, 2023, 10:15 PM +00:00

--- js/typing.js
+++ js/typing.js
@@ -1,20 +1,3 @@
 const textEl = document.getElementById('text')
-const speedEl = document.getElementById('speed')
-const text = "whipped and nae naed"
-let idx = 1
-let speed = 300 / speedEl.value
-speedEl.addEventListener('input', (element) => {
-    speed = 300 / element.target.value
-})
 
-writeText()
-
-function writeText(){
-    textEl.innerText = text.slice(0,idx)
-    idx++
-
-    if (idx > text.length){
-        idx = 1
-    }
-    setTimeout(writeText, speed)
-}
+textEl.innerText = "LKJLKJLKJLKJLKJLKJL"
